12a75acbec7c3622b9c5c8d0a4a33fa04865ded1
Tim Bowersox <11789189+t-bowersox@users.noreply.github.com>
2023-08-05 18:25:10 -0400

    Implement lazy loading for logged-in routes Fixes #19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 import { publicGuard } from './guards/public.guard';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ListPageComponent } from './pages/list-page/list-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 export const routes: Routes = [
   {
@@ -23,13 +21,19 @@ export const routes: Routes = [
   },
   {
     path: 'settings',
-    component: SettingsPageComponent,
+    loadComponent: () =>
+      import('./pages/settings-page/settings-page.component').then(
+        (mod) => mod.SettingsPageComponent,
+      ),
     title: 'Account settings | Goally',
     canActivate: [authGuard],
   },
   {
     path: 'list',
-    component: ListPageComponent,
+    loadComponent: () =>
+      import('./pages/list-page/list-page.component').then(
+        (mod) => mod.ListPageComponent,
+      ),
     title: 'Your list | Goally',
     canActivate: [authGuard],
   },
