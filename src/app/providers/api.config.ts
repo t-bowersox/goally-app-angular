7c9e37a4ba21cb525e1f39b3145bacd49a3518c2
Tim Bowersox <11789189+t-bowersox@users.noreply.github.com>
2023-08-05 15:09:32 -0400

    Fix environment import for api config

--- src/app/providers/api.config.ts
+++ src/app/providers/api.config.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from '@angular/core';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 export const API_CONFIG = new InjectionToken<ApiConfig>('api.config');
 
